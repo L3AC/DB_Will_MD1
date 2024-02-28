812762d66926fd7bef785320c07bda58628b27fd
L3AC <101782981+L3AC@users.noreply.github.com>
2024-02-28 17:21:15 -0600

    Update DML.sql

--- PERFILES/PERFIL 3/DML.sql
+++ PERFILES/PERFIL 3/DML.sql
@@ -1,69 +1,92 @@
 USE libro_express;
 
-/*INSERTAR CLIENTES*/
-CALL InsertarCliente('John Doe', 'johndoe@example.com', '123456789');
-CALL InsertarCliente('Jane Smith', 'janesmith@example.com', '987654321');
-CALL InsertarCliente('Michael Johnson', 'michaeljohnson@example.com', '456789123');
-CALL InsertarCliente('Emily Williams', 'emilywilliams@example.com', '789456123');
-CALL InsertarCliente('David Brown', 'davidbrown@example.com', '321654987');
-CALL InsertarCliente('Sarah Davis', 'sarahdavis@example.com', '654321789');
-CALL InsertarCliente('Matthew Wilson', 'matthewwilson@example.com', '987123456');
-CALL InsertarCliente('Olivia Taylor', 'oliviataylor@example.com', '741852963');
-CALL InsertarCliente('Daniel Anderson', 'danielanderson@example.com', '852963741');
-CALL InsertarCliente('Sophia Martinez', 'sophiamartinez@example.com', '369852147');
-CALL InsertarCliente('Andrew Robinson', 'andrewrobinson@example.com', '258741369');
-CALL InsertarCliente('Isabella Clark', 'isabellaclark@example.com', '147258369');
-CALL InsertarCliente('William Rodriguez', 'williamrodriguez@example.com', '963741852');
-CALL InsertarCliente('Ava Lewis', 'avalewis@example.com', '632587419');
-CALL InsertarCliente('James Lee', 'jameslee@example.com', '419632587');
+/*INSERTAR CLIENTE*/
+CALL InsertCliente('Juanito', 'juanito@gmail.com', '123-4567');
+CALL InsertCliente('Pedrito', 'pedrito@gmail.com', '987-6543');
+CALL InsertCliente('María Elena', 'mariaelena@gmail.com', '555-5678');
+CALL InsertCliente('Carlos Alberto', 'carlosalberto@gmail.com', '555-9012');
+CALL InsertCliente('Ana Sofía', 'anasofia@gmail.com', '555-4321');
+CALL InsertCliente('Miguel Ángel', 'miguelangel@gmail.com', '555-8765');
+CALL InsertCliente('Laura Patricia', 'laurapatricia@gmail.com', '555-0987');
+CALL InsertCliente('Roberto Carlos', 'robertocarlos@gmail.com', '555-1111');
+CALL InsertCliente('Sofía Alejandra', 'sofiaalejandra@gmail.com', '555-2222');
+CALL InsertCliente('Alejandro José', 'alejandrojose@gmail.com', '555-3333');
+CALL InsertCliente('Isabel Cristina', 'isabelcristina@gmail.com', '555-4444');
+CALL InsertCliente('Gabriel Andrés', 'gabrielandres@gmail.com', '555-5555');
+CALL InsertCliente('Lucía Victoria', 'luciavictoria@gmail.com', '555-6666');
+CALL InsertCliente('Luisa Fernanda', 'luisafernanda@gmail.com', '555-7777');
+CALL InsertCliente('Martín Alejandro', 'martinalejandro@gmail.com', '555-8888');
+SELECT * FROM tb_clientes;
 
-/*INSERTAR PRESTAMOS*/
-CALL InsertarPrestamo('e402e8ed-d65a-11ee-8f28-601895385c54', '2024-01-01', '2024-01-07', 'Activo');
-CALL InsertarPrestamo('e680eb6d-d65a-11ee-8f28-601895385c54', '2024-01-02', '2024-01-08', 'Inactivo');
-CALL InsertarPrestamo('e68179b0-d65a-11ee-8f28-601895385c54', '2024-01-03', '2024-01-09', 'Activo');
-CALL InsertarPrestamo('e681f679-d65a-11ee-8f28-601895385c54', '2024-01-04', '2024-01-10', 'Inactivo');
-CALL InsertarPrestamo('e6827bc6-d65a-11ee-8f28-601895385c54', '2024-01-05', '2024-01-11', 'Activo');
-CALL InsertarPrestamo('e682ebd4-d65a-11ee-8f28-601895385c54', '2024-01-06', '2024-01-12', 'Inactivo');
-CALL InsertarPrestamo('e683604b-d65a-11ee-8f28-601895385c54', '2024-01-07', '2024-01-13', 'Activo');
-CALL InsertarPrestamo('e683d190-d65a-11ee-8f28-601895385c54', '2024-01-08', '2024-01-14', 'Inactivo');
-CALL InsertarPrestamo('e68434ee-d65a-11ee-8f28-601895385c54', '2024-01-09', '2024-01-15', 'Activo');
-CALL InsertarPrestamo('e684ab3a-d65a-11ee-8f28-601895385c54', '2024-01-10', '2024-01-16', 'Inactivo');
-CALL InsertarPrestamo('e6851114-d65a-11ee-8f28-601895385c54', '2024-01-11', '2024-01-17', 'Activo');
-CALL InsertarPrestamo('e6856e22-d65a-11ee-8f28-601895385c54', '2024-01-12', '2024-01-18', 'Inactivo');
-CALL InsertarPrestamo('e685bdf3-d65a-11ee-8f28-601895385c54', '2024-01-13', '2024-01-19', 'Activo');
-CALL InsertarPrestamo('e6860a6c-d65a-11ee-8f28-601895385c54', '2024-01-14', '2024-01-20', 'Inactivo');
-CALL InsertarPrestamo('e68657dd-d65a-11ee-8f28-601895385c54', '2024-01-15', '2024-01-21', 'Activo');
+/*INSERTAR PRESTAMO*/
+CALL InsertPrestamo('Juanito', '2024-02-28', '2024-03-05', 'Activo');
+CALL InsertPrestamo('Pedrito', '2024-03-01', '2024-03-15', 'Activo');
+CALL InsertPrestamo('María Elena', '2024-02-28', '2024-03-10', 'Inactivo');
+CALL InsertPrestamo('Carlos Alberto', '2024-03-05', '2024-03-20', 'Activo');
+CALL InsertPrestamo('Ana Sofía', '2024-03-10', '2024-03-25', 'Activo');
+CALL InsertPrestamo('Miguel Ángel', '2024-03-05', '2024-03-15', 'Inactivo');
+CALL InsertPrestamo('Laura Patricia', '2024-03-15', '2024-03-30', 'Activo');
+CALL InsertPrestamo('Roberto Carlos', '2024-03-20', '2024-04-05', 'Activo');
+CALL InsertPrestamo('Sofía Alejandra', '2024-04-01', '2024-04-15', 'Inactivo');
+CALL InsertPrestamo('Alejandro José', '2024-03-25', '2024-04-10', 'Activo');
+CALL InsertPrestamo('Isabel Cristina', '2024-04-10', '2024-04-25', 'Activo');
+CALL InsertPrestamo('Gabriel Andrés', '2024-04-05', '2024-04-15', 'Inactivo');
+CALL InsertPrestamo('Lucía Victoria', '2024-04-15', '2024-04-30', 'Activo');
+CALL InsertPrestamo('Luisa Fernanda', '2024-05-01', '2024-05-15', 'Activo');
+CALL InsertPrestamo('Martín Alejandro', '2024-05-10', '2024-05-25', 'Inactivo');
+SELECT * FROM tb_prestamos;
 
-/*INSERTAR GENERO LIBROS*/
-CALL InsertarGeneroLibro('Fantasía');
-CALL InsertarGeneroLibro('Ciencia ficción');
-CALL InsertarGeneroLibro('Romance');
-CALL InsertarGeneroLibro('Misterio');
-CALL InsertarGeneroLibro('Aventura');
-CALL InsertarGeneroLibro('Histórico');
-CALL InsertarGeneroLibro('Suspenso');
-CALL InsertarGeneroLibro('Drama');
-CALL InsertarGeneroLibro('Comedia');
-CALL InsertarGeneroLibro('Terror');
-CALL InsertarGeneroLibro('Poesía');
-CALL InsertarGeneroLibro('Infantil');
-CALL InsertarGeneroLibro('Autoayuda');
-CALL InsertarGeneroLibro('Biografía');
-CALL InsertarGeneroLibro('Cocina');
+/*INSERTAR GENERO DE LIBRO*/
+CALL InsertGeneroLibro ('Terror');
+CALL InsertGeneroLibro('Ciencia Ficción');
+CALL InsertGeneroLibro('Romance');
+CALL InsertGeneroLibro('Aventura');
+CALL InsertGeneroLibro('Fantasía');
+CALL InsertGeneroLibro('Misterio');
+CALL InsertGeneroLibro('Histórico');
+CALL InsertGeneroLibro('Comedia');
+CALL InsertGeneroLibro('Ciencia');
+CALL InsertGeneroLibro('Drama');
+CALL InsertGeneroLibro('Ficción');
+CALL InsertGeneroLibro('Biografía');
+CALL InsertGeneroLibro('Suspense');
+CALL InsertGeneroLibro('Humor');
+CALL InsertGeneroLibro('Fantasía Épica');
+SELECT * FROM tb_generos_libros;
 
-/*INSERTAR LIBROS */
-CALL InsertarLibro('Libro 1', 2020, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Fantasía'), 'Disponible');
-CALL InsertarLibro('Libro 2', 2018, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Ciencia ficción'), 'Prestado');
-CALL InsertarLibro('Libro 3', 2015, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Romance'), 'Disponible');
-CALL InsertarLibro('Libro 4', 2019, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Misterio'), 'Disponible');
-CALL InsertarLibro('Libro 5', 2021, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Aventura'), 'Prestado');
-CALL InsertarLibro('Libro 6', 2017, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Histórico'), 'Disponible');
-CALL InsertarLibro('Libro 7', 2014, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Suspenso'), 'Disponible');
-CALL InsertarLibro('Libro 8', 2022, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Drama'), 'Prestado');
-CALL InsertarLibro('Libro 9', 2016, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Comedia'), 'Disponible');
-CALL InsertarLibro('Libro 10', 2023, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Terror'), 'Prestado');
-CALL InsertarLibro('Libro 11', 2013, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Poesía'), 'Disponible');
-CALL InsertarLibro('Libro 12', 2011, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Infantil'), 'Disponible');
-CALL InsertarLibro('Libro 13', 2019, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Autoayuda'), 'Prestado');
-CALL InsertarLibro('Libro 14', 2017, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Biografía'), 'Disponible');
-CALL InsertarLibro('Libro 15', 2020, (SELECT id_genero_libro FROM tb_generos_libros WHERE nombre_genero_libro = 'Cocina'), 'Disponible');
+/*INSERTAR LIBRO*/
+CALL InsertLibro ('Harry Potter', 2024, 'Terror', 'Disponible');
+CALL InsertLibro('Dune', 2021, 'Ciencia Ficción', 'Disponible');
+CALL InsertLibro('Orgullo y prejuicio', 1813, 'Romance', 'Prestado');
+CALL InsertLibro('La Odisea', 1813, 'Aventura', 'Disponible');
+CALL InsertLibro('El señor de los anillos', 1954, 'Fantasía', 'Disponible');
+CALL InsertLibro('Sherlock Holmes', 1887, 'Misterio', 'Prestado');
+CALL InsertLibro('Los pilares de la Tierra', 1989, 'Histórico', 'Disponible');
+CALL InsertLibro('La vida de Pi', 2001, 'Aventura', 'Disponible');
+CALL InsertLibro('Cosmos', 1980, 'Ciencia', 'Prestado');
+CALL InsertLibro('Romeo y Julieta', 1597, 'Drama', 'Disponible');
+CALL InsertLibro('1984', 1949, 'Ficción', 'Disponible');
+CALL InsertLibro('Steve Jobs', 2011, 'Biografía', 'Prestado');
+CALL InsertLibro('El Código Da Vinci', 2003, 'Suspense', 'Disponible');
+CALL InsertLibro('El Hobbit', 1937, 'Fantasía Épica', 'Disponible');
+CALL InsertLibro('Humor Sutil', 2005, 'Humor', 'Prestado');
+SELECT * FROM tb_libros;
+
+/*INSERTAR DETALLES DEL PRESTAMO*/
+CALL InsertDetallesPrestamo('Juanito', 'Harry Potter');
+CALL InsertDetallesPrestamo('Luisa Fernanda', 'El Hobbit');
+CALL InsertDetallesPrestamo('Martín Alejandro', 'Humor Sutil');
+CALL InsertDetallesPrestamo('Isabel Cristina', '1984');
+CALL InsertDetallesPrestamo('Gabriel Andrés', 'Steve Jobs');
+CALL InsertDetallesPrestamo('Lucía Victoria', 'El Código Da Vinci');
+CALL InsertDetallesPrestamo('Roberto Carlos', 'La vida de Pi');
+CALL InsertDetallesPrestamo('Sofía Alejandra', 'Cosmos');
+CALL InsertDetallesPrestamo('Alejandro José', 'Romeo y Julieta');
+CALL InsertDetallesPrestamo('Ana Sofía', 'El señor de los anillos');
+CALL InsertDetallesPrestamo('Miguel Ángel', 'Sherlock Holmes');
+CALL InsertDetallesPrestamo('Laura Patricia', 'Los pilares de la Tierra');
+CALL InsertDetallesPrestamo('Pedrito', 'Dune');
+CALL InsertDetallesPrestamo('María Elena', 'Orgullo y prejuicio');
+CALL InsertDetallesPrestamo('Carlos Alberto', 'La Odisea');
+SELECT * FROM tb_detalles_prestamos;
+
