c8c0b9ac7f5c22a48f8a8ad418ea80573a6dfc49
L3AC <101782981+L3AC@users.noreply.github.com>
2024-02-28 09:26:48 -0600

    Update QUERY.sql

--- PERFILES/PERFIL 3/QUERY.sql
+++ PERFILES/PERFIL 3/QUERY.sql
@@ -1,50 +1,52 @@
-DROP DATABASE IF EXIST Ejercicio2;
+DROP DATABASE IF EXISTS Ejercicio2;
 CREATE DATABASE Ejercicio2;
 USE Ejercicio2;
 
-create table clientes ( 
-id_cliente int  primary key,
-nombre_cliente varchar (50),
-email_cliente varchar (100),
-telefono varchar (10)
+CREATE TABLE tb_clientes ( 
+id_cliente INT  PRIMARY KEY,
+nombre_cliente VARCHAR (50),
+email_cliente VARCHAR (100),
+telefono VARCHAR (10)
 );
 
-create table prestamos ( 
-id_prestamo int  primary key,
-id_cliente int,
-fecha_inicio date,
-fecha_devolucion date,
-estado enum('Activo','Inactivo')
-);
-
-create table generos_libros (
-id_genero_libro int primary key,
-nombre_genero_libro varchar(50)
-); 
-
-create table libros (
-id_libro int primary key,
-titulo_libro varchar (50),
-anio_publicacion int,
-id_genero_libro int,
-estado enum ('Disponible', 'Prestado')
-
-
-);
-
-create table detalles_prestamos ( 
-id_detalle_prestamo int  primary key,
-id_prestamo int,
-id_libro int
-);
-
-
-
-CREATE TABLE(
-    ID INT PRIMARY KEY,
-    CONSTRAINT fk_comentario_detalle
-    FOREIGN KEY (id_detalle) REFERENCES prc_detalle_pedidos(id_detalle)
+CREATE TABLE tb_prestamos ( 
+id_prestamo INT  PRIMARY KEY,
+id_cliente INT,
+fecha_inicio DATE,
+fecha_devolucion DATE,
+estado ENUM('Activo','Inactivo'),
+CONSTRAINT fk_prestamo_cliente
+    FOREIGN KEY (id_cliente) REFERENCES tb_clientes(id_detalle)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
-INSERT INTO () VALUES(UUID(),);
+
+CREATE TABLE tb_generos_libros (
+id_genero_libro INT PRIMARY KEY,
+nombre_genero_libro VARCHAR(50)
+); 
+
+CREATE TABLE tb_libros (
+id_libro INT PRIMARY KEY,
+titulo_libro VARCHAR (50),
+anio_publicacion INT,
+id_genero_libro INT,
+estado ENUM ('Disponible', 'Prestado'),
+CONSTRAINT fk_libro_genero
+FOREIGN KEY (id_genero_libro) REFERENCES tb_generos_libros(id_genero_libro)
+ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE tb_detalles_prestamos ( 
+id_detalle_prestamo INT  PRIMARY KEY,
+id_prestamo INT,
+id_libro INT,
+CONSTRAINT fk_detalle_prestamo
+FOREIGN KEY (id_prestamo) REFERENCES tb_prestamos(id_prestamo)
+ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT fk_detalle_libro
+FOREIGN KEY (id_libro) REFERENCES tb_libros(id_libro)
+ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
 
